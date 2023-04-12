917eb1fbd03f959f6b0e0f1d7f7a7e8347812cc4
chxry <chxry@floppa.world>
2023-04-12 21:19:10 +0100

    a teeny tiny amount of testing

--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,79 @@
-use bonsaidb::core::connection::Connection;
-use bonsaidb::core::schema::{Collection, Schema};
-use bonsaidb::local::config::{Builder, StorageConfiguration};
-use bonsaidb::local::Database;
-use bonsaidb_core::schema::SerializedCollection;
+mod state;
+
+use tide::{Request, Response, StatusCode};
+use tide::security::CorsMiddleware;
 use tide::http::headers::HeaderValue;
 use tide::http::Cookie;
-use tide::prelude::*;
-use tide::security::CorsMiddleware;
-use tide::{Request, Response, StatusCode};
 use uuid::Uuid;
-pub type Result<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+use serde::Deserialize;
+use state::{State, User};
 
-#[derive(Debug, Schema)]
-#[schema(name = "userdb", collections = [User,Session])]
-struct UserDB;
+pub type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-#[derive(Debug, Serialize, Deserialize, Collection, Eq, PartialEq)]
-#[collection(name = "users",primary_key = String)]
-struct User {
-    password: String,
+#[derive(Deserialize)]
+struct UserBody {
+  username: String,
+  password: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Collection, Eq, PartialEq)]
-#[collection(name = "session",primary_key = Uuid)]
-struct Session {
-    uid: String,
-}
-#[derive(Deserialize, Serialize)]
-struct LoginUser {
-    username: String,
-    password: String,
-}
-#[derive(Clone)]
-struct State {
-    db: bonsaidb_local::Database,
-}
-
-impl State {
-    async fn new() -> Result<Self> {
-        let dab = Database::open::<UserDB>(StorageConfiguration::new("based.bonsaidb"))?;
-        Ok(Self { db: dab })
-    }
-}
-
-#[async_std::main]
+#[tokio::main]
 async fn main() -> Result {
-    tide::log::start();
-    let mut app = tide::with_state(State::new().await?);
-    app.with(CorsMiddleware::new().allow_methods("GET, POST".parse::<HeaderValue>()?));
-    app.at("/register").post(new);
-    app.at("/login").post(login);
-    app.at("/logout").post(logout);
-    app.listen("127.0.0.1:8080").await?;
-    Ok(())
+  tide::log::start();
+  let mut app = tide::with_state(State::load()?);
+  app.with(CorsMiddleware::new().allow_methods("GET, POST".parse::<HeaderValue>()?));
+  app.at("/register").post(new);
+  app.at("/login").post(login);
+  app.at("/logout").post(logout);
+  app.listen("127.0.0.1:8080").await?;
+  Ok(())
 }
 
 async fn new(mut req: Request<State>) -> tide::Result {
-    let mut user: LoginUser = req.body_form().await?;
-    match User::get(&user.username, &req.state().db)? {
-        Some(_) => Ok("uwer exists uwu".into()),
-        None => {
-            user.password = bcrypt::hash(user.password, 10)?;
-            req.state().db.collection::<User>().insert(
-                &user.username,
-                &User {
-                    password: user.password,
-                },
-            )?;
-            Ok("user created".into())
-        }
+  let user: UserBody = req.body_form().await?;
+  let mut state = req.state().lock();
+  match state.users.get(&user.username) {
+    Some(_) => Ok("uwer exists uwu".into()),
+    None => {
+      state.users.insert(
+        user.username,
+        User {
+          password: bcrypt::hash(user.password, 10)?,
+        },
+      );
+      Ok("user created".into())
     }
+  }
 }
 
 async fn login(mut req: Request<State>) -> tide::Result {
-    let user: LoginUser = req.body_form().await?;
-    match User::get(&user.username, &req.state().db)? {
-        Some(u) => {
-            if bcrypt::verify(user.password, &u.contents.password)? {
-                let mut res = Response::new(StatusCode::Ok);
-                let fart = Uuid::new_v4();
-                req.state()
-                    .db
-                    .collection::<Session>()
-                    .insert(&fart, &Session { uid: user.username })?;
-                res.insert_cookie(Cookie::new("session", fart.to_string()));
-                Ok(res)
-            } else {
-                Ok("not fart".into())
-            }
-        }
-        None => Ok("user no exist".into()),
+  let user: UserBody = req.body_form().await?;
+  let mut state = req.state().lock();
+  match state.users.get(&user.username) {
+    Some(u) => {
+      if bcrypt::verify(user.password, &u.password)? {
+        let mut res = Response::new(StatusCode::Ok);
+        let id = Uuid::new_v4();
+        state.sessions.insert(id, user.username);
+        res.insert_cookie(Cookie::new("session", id.to_string()));
+        Ok(res)
+      } else {
+        Ok("not fart".into())
+      }
     }
+    None => Ok("user no exist".into()),
+  }
 }
 
 async fn logout(req: Request<State>) -> tide::Result {
-    let db = req.state().db.collection::<Session>();
-    match db.get(&Uuid::parse_str(req.cookie("session").unwrap().value())?)? {
-        Some(doc) => {
-            db.delete(&doc)?;
-            let mut res = Response::new(StatusCode::Ok);
-            res.remove_cookie(Cookie::named("session"));
-            Ok(res)
-        }
-        None => Ok("not ok".into()),
+  let mut state = req.state().lock();
+  let id = Uuid::parse_str(req.cookie("session").unwrap().value())?;
+  match state.sessions.get(&id) {
+    Some(_) => {
+      state.sessions.remove(&id);
+      let mut res = Response::new(StatusCode::Ok);
+      res.remove_cookie(Cookie::named("session"));
+      Ok(res)
     }
+    None => Ok("not ok".into()),
+  }
 }
---
+++ src/state.rs
@@ -0,0 +1,60 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::sync::{Arc, Mutex, MutexGuard};
+use std::ops::{Deref, DerefMut};
+use tide::log::info;
+use uuid::Uuid;
+use serde::{Serialize, Deserialize};
+use crate::Result;
+
+const PATH: &str = "auth.db";
+
+#[derive(Clone)]
+pub struct State(Arc<Mutex<StateInner>>);
+
+impl State {
+  pub fn load() -> Result<Self> {
+    Ok(Self(Arc::new(Mutex::new(match File::open(PATH) {
+      Ok(f) => bincode::deserialize_from(f)?,
+      Err(_) => StateInner::default(),
+    }))))
+  }
+
+  pub fn lock(&self) -> StateGuard {
+    StateGuard(self.0.lock().unwrap())
+  }
+}
+
+#[derive(Clone, Serialize, Deserialize, Default)]
+pub struct StateInner {
+  pub sessions: HashMap<Uuid, String>,
+  pub users: HashMap<String, User>,
+}
+
+pub struct StateGuard<'a>(MutexGuard<'a, StateInner>);
+
+impl Deref for StateGuard<'_> {
+  type Target = StateInner;
+
+  fn deref(&self) -> &StateInner {
+    &*self.0
+  }
+}
+
+impl DerefMut for StateGuard<'_> {
+  fn deref_mut(&mut self) -> &mut StateInner {
+    &mut *self.0
+  }
+}
+
+impl Drop for StateGuard<'_> {
+  fn drop(&mut self) {
+    info!("Saving {}", PATH);
+    bincode::serialize_into(File::create(PATH).unwrap(), &*self.0).unwrap();
+  }
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct User {
+  pub password: String,
+}
